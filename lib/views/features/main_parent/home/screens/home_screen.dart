1c7feabfd90b717f6f522eed254d87d5658e7bdf
Zaman Sheikh <zaman6545@gmail.com>
2025-06-18 11:00:55 +0600

    feat: Implement dynamic greeting based on time of day in HomeScreen

--- lib/views/features/main_parent/home/screens/home_screen.dart
+++ lib/views/features/main_parent/home/screens/home_screen.dart
@@ -73,7 +73,7 @@ class HomeScreen extends StatelessWidget {
               style: TextStyle(color: textSecondary, fontSize: 16),
             ),
             Text(
-              'Good Morning',
+              _getTimeBasedGreeting(),
               style: TextStyle(
                 color: textPrimary,
                 fontSize: 18.sp,
@@ -947,5 +947,20 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  String _getTimeBasedGreeting() {
+    final now = DateTime.now();
+    final hour = now.hour;
+
+    if (hour >= 5 && hour < 12) {
+      return 'Good Morning';
+    } else if (hour >= 12 && hour < 17) {
+      return 'Good Afternoon';
+    } else if (hour >= 17 && hour < 21) {
+      return 'Good Evening';
+    } else {
+      return 'Good Night';
+    }
   }
 }
